4f1d53a63662aa11d0c44443002f37276f6616fc
mveron77 <mveron.work77@gmail.com>
2019-11-11 23:32:00 -0300

    subiendo aplicacion de autos parcial

---
+++ CarApp/src/main/java/com/practice/java/CarApp/controller/Auto.java
@@ -0,0 +1,29 @@
+package com.practice.java.CarApp.controller;
+
+public class Auto {
+	private String name;
+	private String age;
+	
+	
+	public Auto(String name, String age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
+}
